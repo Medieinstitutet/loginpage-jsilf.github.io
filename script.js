cea47244df80df720d56b13dfd79d9271deb3dc0
Jessica <92667872+jsilf@users.noreply.github.com>
2021-12-01 13:19:29 +0100

    WIP first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("hello world");
+
+let header = document.getElementById("header");
+let inputName = document.getElementById("inputName");
+let inputPsw = document.getElementById("inputPsw");
+let loginBtn = document.getElementById("loginBtn");
+let content = document.getElementById("content");
+
+
+/* Header icke inloggad startsida */
+/* Content icke inloggad startsida */
+
+/* Content fel inloggning */
+
+/* Header inloggad välkommen */
+/* Content inloggad välkommen*/
+
+function login() {
+
+}
+
+
